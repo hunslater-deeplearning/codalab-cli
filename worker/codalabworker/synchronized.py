e26115e8e91e16c707cc4158b36298969a6c02f2
Fabian Chan <fabianc@stanford.edu>
2018-04-01 20:37:50 +0900

    Replaced synchronized

--- worker/codalabworker/synchronized.py
+++ worker/codalabworker/synchronized.py
@@ -2,12 +2,42 @@
 Java-like synchronization primitives in Python
 
 source: http://theorangeduck.com/page/synchronized-python
+http://blog.dscpl.com.au/2014/01/the-synchronized-decorator-as-context.html
 """
 
 import thread
 import threading
 import types
 
+def synchronized(wrapped):
+    def _synchronized_lock(owner):
+        lock = vars(owner).get('_synchronized_lock', None)
+        if lock is None:
+            meta_lock = vars(synchronized).setdefault(
+                    '_synchronized_meta_lock', threading.Lock())
+            with meta_lock:
+                lock = vars(owner).get('_synchronized_lock', None)
+                if lock is None:
+                    lock = threading.RLock()
+                    setattr(owner, '_synchronized_lock', lock)
+        return lock
+
+    def _synchronized_wrapper(wrapped, instance, args, kwargs):
+        with _synchronized_lock(instance or wrapped):
+            return wrapped(*args, **kwargs)
+
+    class _synchronized_function_wrapper(function_wrapper):
+        def __enter__(self):
+            self._lock = _synchronized_lock(self.wrapped)
+            self._lock.acquire()
+            return self._lock
+
+        def __exit__(self, *args):
+            self._lock.release()
+
+    return _synchronized_function_wrapper(wrapped, _synchronized_wrapper)
+
+'''
 def synchronized_with_attr(lock_name):
 
     def decorator(method):
@@ -68,3 +98,4 @@ def synchronized(item):
         new_lock = threading.Lock()
         decorator = syncronized_with(new_lock)
         return decorator(item)
+'''
